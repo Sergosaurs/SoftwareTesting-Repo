c580a76af21d5686ec7dc02756ad3c81e6081f68
Serge Macovei <greserega@gmail.com>
2021-11-14 00:14:31 +0300

    litecart - login to the administration panel

---
+++ src/test/java/ru/makj/mavenproject/LiteCart.java
@@ -0,0 +1,37 @@
+package ru.makj.mavenproject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LiteCart {
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void runTest() {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.className("name")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.close();
+    }
+}
