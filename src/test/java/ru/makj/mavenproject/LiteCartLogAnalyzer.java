387a4224b45e89de8a05ac677edf2963e498363a
Serge Macovei <greserega@gmail.com>
2021-12-07 00:01:31 +0300

    added one more log check

--- src/test/java/ru/makj/mavenproject/LiteCartLogAnalyzer.java
+++ src/test/java/ru/makj/mavenproject/LiteCartLogAnalyzer.java
@@ -14,9 +14,35 @@ import java.util.List;
 import java.util.Objects;
 import java.util.logging.Level;
 
+import static org.junit.Assert.assertEquals;
+
 public class LiteCartLogAnalyzer extends AppTest {
 
-    public LogEntries logEntries;
+    List<LogEntry> logEntryList;
+    LogEntries logEntries;
+
+    @Test
+    public void shouldBeNoMessagesInBrowserLog() {
+        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=2");
+        loginAsAdmin();
+        List<WebElement> productList = driver.findElements(By.cssSelector(".dataTable tr.row"));
+
+        for (int i = 3; i < productList.size(); i++) {
+            productList = driver.findElements(By.cssSelector(".dataTable tr.row"));
+            productList.get(i).findElement(By.tagName("a")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content h1")));
+            try {
+                logEntryList = driver.manage().logs().get("browser").getAll();
+                assertEquals(0, logEntryList.size());
+            } catch (AssertionError error) {
+                System.err.println("WARNING: Found browser logs on the current page!: ");
+                logEntryList.forEach(System.out::println);
+                System.out.println("\n");
+                driver.navigate().back();
+            }
+        }
+        driver.quit();
+    }
 
     @Test
     public void productLogsChecking() {
